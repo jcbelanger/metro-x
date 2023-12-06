3357e91187a06981a793b0bc17e4222e25f87ecc
Joshua Belanger <jbelanger@penbaytechgroup.com>
2023-12-06 00:21:08 -0500

    cleanup vite github build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => ({
   plugins: [react()],
-  base: '/metro-x/'
-})
+  base: command == 'build' ? '/metro-x/' : '/'
+}));
