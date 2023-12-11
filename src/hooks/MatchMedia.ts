df0ea56c4bae880a857d3b4a5d96432108c403f4
Joshua Belanger <jbelanger@penbaytechgroup.com>
2023-12-11 11:08:53 -0500

    cleanup mql hook

--- src/hooks/MatchMedia.ts
+++ src/hooks/MatchMedia.ts
@@ -1,17 +1,15 @@
-import { useLayoutEffect, useState, DependencyList } from "react";
+import { useLayoutEffect, useState } from "react";
 
 
-export default function useMatchMediaQuery(mediaQuery:string, deps?: DependencyList):boolean {
-    const intialMatch = window.matchMedia(mediaQuery).matches;
-    const [matches, setMatches] = useState(intialMatch);
+export default function useMatchMediaQuery(mediaQuery:string):boolean {
+    const [matches, setMatches] = useState(window.matchMedia(mediaQuery).matches);
     
     useLayoutEffect(() => {
-      const handleMQChange = (event:MediaQueryListEvent) => setMatches(event.matches);
       const mql = window.matchMedia(mediaQuery);
-      mql.addEventListener("change", handleMQChange);
-      return () => mql.removeEventListener("change", handleMQChange);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, deps);
+      const handleMqlChange = (event:MediaQueryListEvent) => setMatches(event.matches);
+      mql.addEventListener("change", handleMqlChange);
+      return () => mql.removeEventListener("change", handleMqlChange);
+    }, [mediaQuery]);
 
     return matches;
 }
